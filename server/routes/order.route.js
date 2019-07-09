f99b40eb1e9953c585dd5de03b1ff78124d77551
Mykhailo Odrekhivskyi <skipokylas@gmail.com>
2019-07-09 23:22:10 +0300

    added order API

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
 const controllers = require('../controllers/order.controller');
 
 const router = express.Router();
 
-router.get('/', controllers.getAll);
-router.post('/', controllers.create);
+router.get('/', passport.authenticate('jwt', { session: false }), controllers.getAll);
+router.post('/', passport.authenticate('jwt', { session: false }), controllers.create);
 
 module.exports = router;
