e83c8a59bd5c0c5eaa952c3f96489f9f4cf3ddd4
Mykhailo Odrekhivskyi <skipokylas@gmail.com>
2019-07-11 23:20:04 +0300

    added login form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +16,7 @@ import { RegisterPageComponent } from './register-page/register-page.component';
     SiteLayoutComponent,
     RegisterPageComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
