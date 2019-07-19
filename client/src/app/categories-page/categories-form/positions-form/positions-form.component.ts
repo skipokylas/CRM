63e0d93168219efa00a2d117567b3bc654619f30
Mykhailo Odrekhivskyi <skipokylas@gmail.com>
2019-07-19 23:13:12 +0300

    added logic for editing and deleting position

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input, ViewChild, OnDestroy } from '@angular/core';
 import { PositionService } from 'src/app/shared/services/position.service';
-import { IPosition, MaterialModalInstance } from 'src/app/shared/interfaces';
+import { IPosition, MaterialModalInstance, IMessage } from 'src/app/shared/interfaces';
 import { MaterialService } from 'src/app/shared/helpers/materialize.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-positions-form',
@@ -14,11 +16,14 @@ export class PositionsFormComponent implements OnInit, OnDestroy {
 
   positions: IPosition[] = [];
   loading = false;
+  positionId = null;
   modal: MaterialModalInstance;
+  form: FormGroup;
 
   constructor(private positionService: PositionService) {}
 
   ngOnInit() {
+    this.initForm();
     this.loading = true;
     this.positionService.fetch(this.categoryId).subscribe(positions => {
       this.positions = positions;
@@ -28,19 +33,106 @@ export class PositionsFormComponent implements OnInit, OnDestroy {
     this.modal = MaterialService.initModal(this.modalRef);
   }
 
-  ngOnDestroy() {
+  initForm(): void {
+    this.form = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      cost: new FormControl(1, [Validators.required, Validators.min(1)])
+    });
+  }
+
+  ngOnDestroy(): void {
     this.modal.destroy();
   }
 
-  onSelectPosion(position: IPosition) {
+  onSelectPosion(position: IPosition): void {
+    this.positionId = position._id;
+    this.form.patchValue({
+      name: position.name,
+      cost: position.cost
+    });
     this.modal.open();
+    MaterialService.UpdateTextInputs();
   }
 
-  onAddPosition() {
+  onAddPosition(): void {
+    this.positionId = null;
+    this.form.reset({
+      name: null,
+      cost: 1
+    });
     this.modal.open();
+    MaterialService.UpdateTextInputs();
   }
 
-  onCancel() {
+  onCancel(): void {
     this.modal.close();
+  }
+
+  onDelete(event: Event, position: IPosition): void {
+    event.stopPropagation();
+    const decision = window.confirm(`Do you really want to delete: ${position.name}`);
+    if (decision) {
+      this.positionService.delete(position).subscribe(
+        (response: IMessage) => {
+          this.positions.splice(this.findIndex(position), 1);
+          MaterialService.toast(response.message);
+        },
+        error => {
+          MaterialService.toast(error.error.message);
+        }
+      );
+    }
+  }
+
+  onSubmit(): void {
+    this.form.disable();
+    const position: IPosition = {
+      name: this.form.value.name,
+      cost: this.form.value.cost,
+      category: this.categoryId
+    };
+
+    this.positionId ? this.updatePosition(position) : this.createPosition(position);
+  }
+
+  createPosition(position: IPosition) {
+    this.positionService.create(position).subscribe(
+      (createdPosition: IPosition) => {
+        MaterialService.toast(`Position: ${createdPosition.name} was updated`);
+        this.positions.push(createdPosition);
+      },
+      (error: HttpErrorResponse) => {
+        this.form.enable();
+        MaterialService.toast(error.error.message);
+      },
+      () => {
+        this.modal.close();
+        this.form.reset({ name: null, cost: 1 });
+        this.form.enable();
+      }
+    );
+  }
+
+  updatePosition(position: IPosition) {
+    position._id = this.positionId;
+    this.positionService.update(position).subscribe(
+      (updatedPosition: IPosition) => {
+        MaterialService.toast(`Position: ${updatedPosition.name} was created`);
+        this.positions[this.findIndex(position)] = position;
+      },
+      (error: HttpErrorResponse) => {
+        this.form.enable();
+        MaterialService.toast(error.error.message);
+      },
+      () => {
+        this.modal.close();
+        this.form.reset({ name: null, cost: 1 });
+        this.form.enable();
+      }
+    );
+  }
+
+  findIndex(position: IPosition): number {
+    return this.positions.findIndex((p: IPosition) => p._id === position._id);
   }
 }
--- client/src/app/shared/services/position.service.ts
+++ client/src/app/shared/services/position.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IPosition } from '../interfaces';
+import { IPosition, IMessage } from '../interfaces';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
@@ -9,5 +9,17 @@ export class PositionService {
 
   fetch(categoryId: string): Observable<IPosition[]> {
     return this.http.get<IPosition[]>(`/api/position/${categoryId}`);
+  }
+
+  create(position: IPosition): Observable<IPosition> {
+    return this.http.post<IPosition>('/api/position', position);
+  }
+
+  update(position: IPosition): Observable<IPosition> {
+    return this.http.patch<IPosition>(`/api/position/${position._id}`, position);
+  }
+
+  delete(position: IPosition): Observable<IMessage> {
+    return this.http.delete<IMessage>(`/api/position/${position._id}`);
   }
 }
