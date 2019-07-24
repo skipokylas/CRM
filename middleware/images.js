eb8d5a557533a995b1b2ad4404a8e8a7f43d4470
Mykhailo Odrekhivskyi <skipokylas@gmail.com>
2019-07-24 22:22:50 +0300

    changed folder structure

--- middleware/images.js
+++ middleware/images.js
@@ -5,7 +5,7 @@ const config = require('../config/config');
 const storage = new Storage({
     projectId: config.google.projectId,
     credentials: {
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
     },
 });
