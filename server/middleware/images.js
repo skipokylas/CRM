7a533fa1b7a56a8242cbd4a059055958d94e10de
Mykhailo Odrekhivskyi <skipokylas@gmail.com>
2019-07-24 19:49:00 +0300

    updated config files

--- server/middleware/images.js
+++ server/middleware/images.js
@@ -4,7 +4,10 @@ const config = require('../config/config');
 
 const storage = new Storage({
     projectId: config.google.projectId,
-    keyFilename: './config/keyfile.json',
+    credentials: {
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    },
 });
 
 const bucket = storage.bucket(config.google.bucket);
